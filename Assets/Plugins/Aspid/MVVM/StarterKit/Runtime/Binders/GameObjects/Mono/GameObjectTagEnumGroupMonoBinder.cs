a7f5b7e8de64e2c7a404e3b36359fffaf8001711
Vladislav Panin <vpd-2000@yandex.com>
2024-12-13 21:34:03 +0300

    Update GameObject Binders

--- Assets/Plugins/Aspid/MVVM/StarterKit/Runtime/Binders/GameObjects/Mono/GameObjectTagEnumGroupMonoBinder.cs
+++ Assets/Plugins/Aspid/MVVM/StarterKit/Runtime/Binders/GameObjects/Mono/GameObjectTagEnumGroupMonoBinder.cs
@@ -3,16 +3,9 @@ using UnityEngine;
 namespace Aspid.MVVM.StarterKit.Binders.Mono
 {
     [AddComponentMenu("Binders/GameObject/GameObject Binder - Tag Enum Group")]
-    public sealed class GameObjectTagEnumGroupMonoBinder : EnumGroupMonoBinder<GameObject>
+    public sealed class GameObjectTagEnumGroupMonoBinder : EnumGroupMonoBinder<GameObject, string>
     {
-        [Header("Parameters")]
-        [SerializeField] private string _defaultValue;
-        [SerializeField] private string _selectedValue;
-        
-        protected override void SetDefaultValue(GameObject component) =>
-            component.tag = _defaultValue;
-
-        protected override void SetSelectedValue(GameObject component) =>
-            component.tag = _selectedValue;
+        protected override void SetValue(GameObject component, string value) =>
+            component.tag = value;
     }
 }

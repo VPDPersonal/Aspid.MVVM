fa2bfb0437acb00e90b20c0ddc3676703aabbfff
Vladislav Panin <vpd-2000@yandex.com>
2025-01-06 17:17:02 +0300

    Upgrade MVVM Framework

---
+++ Assets/Plugins/Aspid/MVVM/Source/Runtime/Generation/BindIdAttribute.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Aspid.MVVM.Generation
+{
+    /// <summary>
+    /// Attribute used to override the binding ID for fields, properties, or [RelayCommand] in a ViewModel and View.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
+    public sealed class BindIdAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BindIdAttribute"/> class with a specified ID.
+        /// </summary>
+        /// <param name="id">The binding ID to be associated with the target field, property, or [RelayCommand].</param>
+        public BindIdAttribute(string id) { }
+    }
+}

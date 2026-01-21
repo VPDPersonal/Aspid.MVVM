6d69a9ae0b453daa7bdd1264f428e19c33c8dd2d
Vladislav Panin <vpd-2000@yandex.com>
2026-01-21 09:27:06 +0300

    Fix HelpBoxExtensions

--- Aspid.MVVM/Assets/Plugins/Aspid/UnityFastTools/Source/Runtime/VisualElements/Extensions/HelpBoxExtensions.cs
+++ Aspid.MVVM/Assets/Plugins/Aspid/UnityFastTools/Source/Runtime/VisualElements/Extensions/HelpBoxExtensions.cs
@@ -1,12 +1,11 @@
 using UnityEngine.UIElements;
 
-// TODO Aspid.UnityFastTools
 // ReSharper disable CheckNamespace
 namespace Aspid.UnityFastTools
 {
     public static class HelpBoxExtensions
     {
-        public static T SetFontSize<T>(this T helpBox, StyleLength value)
+        public static T SetHelpBoxFontSize<T>(this T helpBox, StyleLength value)
             where T : HelpBox
         {
             helpBox.Q<Label>().SetFontSize(value);

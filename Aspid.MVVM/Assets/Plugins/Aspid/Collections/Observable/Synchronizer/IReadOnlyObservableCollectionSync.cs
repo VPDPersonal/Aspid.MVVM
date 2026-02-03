783bd3eea1eac59a4a883078426d0bcf9d4fb552
Vladislav Panin <vpd.aspid@gmail.com>
2026-02-03 14:25:30 +0300

    Remove Collections

--- Aspid.MVVM/Assets/Plugins/Aspid/Collections/Observable/Synchronizer/IReadOnlyObservableCollectionSync.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-// ReSharper disable once CheckNamespace
-namespace Aspid.Collections.Observable.Synchronizer
-{
-    public interface IReadOnlyObservableCollectionSync<out T> : IObservableCollection<T>, IDisposable { }
-}

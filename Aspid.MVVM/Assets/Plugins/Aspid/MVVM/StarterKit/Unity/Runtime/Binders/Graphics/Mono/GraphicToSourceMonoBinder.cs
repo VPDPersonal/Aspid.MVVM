5a7b3a0f8f0349c909c3e38b645e0a6e2da4ad4d
Vladislav Panin <vpd.aspid@gmail.com>
2026-03-08 19:50:32 +0300

    Fix Graphic To Source Mono Binder Summary

--- Aspid.MVVM/Assets/Plugins/Aspid/MVVM/StarterKit/Unity/Runtime/Binders/Graphics/Mono/GraphicToSourceMonoBinder.cs
+++ Aspid.MVVM/Assets/Plugins/Aspid/MVVM/StarterKit/Unity/Runtime/Binders/Graphics/Mono/GraphicToSourceMonoBinder.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 namespace Aspid.MVVM.StarterKit
 {
     /// <summary>
-    /// MonoBehaviour binder that reads <see cref="Graphic"/> properties back to the ViewModel in one-way-to-source mode.
+    /// MonoBehaviour binder that reads the current state of a <see cref="Graphic"/> component
+    /// back to the ViewModel in one-way-to-source mode when binding is established.
     /// </summary>
     [AddBinderContextMenu(typeof(Graphic))]
     [AddComponentMenu("Aspid/MVVM/Binders/UI/Graphic/Graphic To Source Binder")]

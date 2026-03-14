8facb0dcf10f203a0c60a45de99ed837d56b86d0
Vladislav Panin <vpd.aspid@gmail.com>
2026-03-14 18:48:51 +0300

    Remove

--- Aspid.MVVM/Assets/Plugins/Aspid/MVVM/StarterKit/Unity/Runtime/Converters/Vectors/TransformPositionCombineConverter.cs
+++ Aspid.MVVM/Assets/Plugins/Aspid/MVVM/StarterKit/Unity/Runtime/Converters/Vectors/TransformPositionCombineConverter.cs
@@ -1,10 +1,5 @@
 using System;
 using UnityEngine;
-#if UNITY_2023_1_OR_NEWER
-using Converter = Aspid.MVVM.StarterKit.IConverter<UnityEngine.Vector3, UnityEngine.Vector3>;
-#else
-using Converter = Aspid.MVVM.StarterKit.IConverterVector3;
-#endif
 
 // ReSharper disable once CheckNamespace
 namespace Aspid.MVVM.StarterKit

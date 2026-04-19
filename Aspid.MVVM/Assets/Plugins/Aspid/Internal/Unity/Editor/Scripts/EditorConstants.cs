15c6d31bd815f494a7912d54b598fd7fcf048351
Vladislav Panin <vpd.aspid@gmail.com>
2026-04-19 13:19:20 +0300

    Update meta files

--- Aspid.MVVM/Assets/Plugins/Aspid/Internal/Unity/Editor/Scripts/EditorConstants.cs
+++ Aspid.MVVM/Assets/Plugins/Aspid/Internal/Unity/Editor/Scripts/EditorConstants.cs
@@ -5,9 +5,9 @@ namespace Aspid.Internal
 {
     public static class EditorConstants
     {
-        public const string AspidIconRed = "AspidIconRed";
-        public const string AspidIconGreen = "AspidIconGreen";
-        public const string AspidIconYellow = "AspidIconYellow";
+        public const string AspidIconRed = "aspid_icon_medium_red_1020x1008";
+        public const string AspidIconGreen = "aspid_icon_medium_green_1020x1008";
+        public const string AspidIconYellow = "aspid_icon_medium_yellow_1020x1008";
 
         public static readonly Color ErrorColor = new(r: 0.41f, g: 0.05f, b: 0.05f);
         public static readonly Color WarningColor = new(r: 0.61f, g: 0.44f, b: 0.11f);
